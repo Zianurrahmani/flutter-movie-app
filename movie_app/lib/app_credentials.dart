1439c282424d2aacefa200498346fc166b2140bf
Zianurrahmani <nurrahmani.zian@gmail.com>
2022-12-28 22:41:40 +0700

    setup done

---
+++ movie_app/lib/app_credentials.dart
@@ -0,0 +1,6 @@
+class AppUrl {
+  static const String baseUrl = "https://api.themoviedb.org/3";
+  static const String baseImageUrl = "https://image.tmdb.org/t/p/w500";
+  static const String baseImageOriUrl = "https://image.tmdb.org/t/p/original";
+  static const String apikey = "cc30d9235c73a52aed67fc5ddaf12e89";
+}
